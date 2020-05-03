bab3f27ba6dd34fd46828b5327724cf05166bc8d
IPogorelova <pisseversk@mail.ru>
2020-05-04 02:42:19 +0300

    #WIP: footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,56 +1,57 @@
 import * as React from 'react'
-import './Footer.scss'
 
 const Footer = () => {
 
   return (
     <footer className='footer'>
-      <div className='footer__row'>
-        <img src='' alt='JetBrains logo'/>
-        <div className='footer__col'>
-          <ul>
-            About JetBrains
-            <li>
-              <a href='#'>Company</a>
-            </li>
-            <li>
-              <a href='#'>Contacts</a>
-            </li>
-            <li>
-              <a href='#'>Press & Logos</a>
-            </li>
-            <li>
-              <a href='#'>Careers</a>
-            </li>
-          </ul>
+      <div className="container">
+        <div className='footer__row'>
+          <img className='footer__logo' src='' alt='JetBrains logo'/>
+          <div className='footer__col'>
+            <span className='footer__subtitle'>About JetBrains</span>
+            <ul className='footer__nav'>
+              <li>
+                <a href='#' className='footer__link'>Company</a>
+              </li>
+              <li>
+                <a href='#' className='footer__link'>Contacts</a>
+              </li>
+              <li>
+                <a href='#' className='footer__link'>Press & Logos</a>
+              </li>
+              <li>
+                <a href='#' className='footer__link'>Careers</a>
+              </li>
+            </ul>
+          </div>
+          <div className='footer__col'>
+            <span className='footer__subtitle'>Follow JetBrains</span>
+            <ul className='footer__socials'>
+              <li>
+                <a className='footer__icon' href='#' title='JetBrains on Facebook'/>
+              </li>
+              <li>
+                <a className='footer__icon' href='#' title='JetBrains on Twitter'/>
+              </li>
+              <li>
+                <a className='footer__icon' href='#' title='JetBrains on LinkedIn'/>
+              </li>
+              <li>
+                <a className='footer__icon' href='#' title='JetBrains on YouTube'/>
+              </li>
+              <li>
+                <a className='footer__icon' href='#' title='JetBrains blog'></a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className='footer__col'>
-          <ul>
-            Follow JetBrains
-            <li>
-              <a href='#' title='Facebook'></a>
-            </li>
-            <li>
-              <a href='#' title='Twitter'></a>
-            </li>
-            <li>
-              <a href='#' title='LinkedIn'></a>
-            </li>
-            <li>
-              <a href='#' title='YouTube'></a>
-            </li>
-            <li>
-              <a href='#' title='Blog'></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='footer__row'>
-        <span>copyright</span>
-        <span>developed</span>
-        <div className="footer__privacy">
-          <a href="#">Privacy & Security</a>
-          <a href="#">Terms of Use</a>
+        <div className='footer__row'>
+          <span>copyright</span>
+          <span>developed</span>
+          <div className="footer__privacy">
+            <a href="#">Privacy & Security</a>
+            <a href="#">Terms of Use</a>
+          </div>
         </div>
       </div>
     </footer>
