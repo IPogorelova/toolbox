486f0f3b622bbe9a7ae636c63d5cd672af4efd78
IPogorelova <pisseversk@mail.ru>
2020-05-06 12:41:41 +0300

    start Contacts block

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <ToolboxApp/>
         {/*<Users users={usersList}/>*/}
         <FAQ/>
-        {/*<Contacts/>*/}
+        <Contacts/>
       </main>
       <Footer/>
     </div>
